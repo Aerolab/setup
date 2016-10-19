f8ecdf01bbcaa146f3dedcc4cf6eb09b82532f17
Roberto González <roberto@aerolab.com.ar>
2016-10-19 14:40:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -165,6 +165,7 @@ if options['designer'] == 'y':
   print "Installing Designer Tools"
   os.system('brew cask install invisionsync iconjar skala-preview lingo')
   os.system('brew cask install adapter handbrake')
+  show_notification("We need your password")
   os.system('brew cask install origami')
 
 
