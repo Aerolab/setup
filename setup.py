ed6059892e0e75a38df8e0495c1751601dd23b3e
Roberto Gonzalez <roberto@aerolab.co>
2020-05-07 13:13:35 -0300

    Catalina fix

--- setup.py
+++ setup.py
@@ -122,6 +122,8 @@ os.system('npm install -g yo gulp-cli node-gyp serve ndb')
 # OSX Tweaks & Essentials
 print "Installing Quicklook Helpers"
 os.system('brew cask install qlcolorcode qlstephen qlmarkdown quicklook-csv quicklook-json webpquicklook suspicious-package quicklookase qlvideo qlimagesize epubquicklook qlprettypatch qlvideo')
+# Permissions Fix for macOS Catalina
+os.system('xattr -d -r com.apple.quarantine ~/Library/QuickLook')
 
 print "Installing Fonts"
 os.system('brew cask install font-dosis font-droid-sans-mono-for-powerline font-open-sans font-open-sans-condensed font-roboto font-roboto-mono font-roboto-condensed font-roboto-slab font-consolas-for-powerline font-dejavu-sans font-dejavu-sans-mono-for-powerline font-inconsolata font-inconsolata-for-powerline font-lato font-menlo-for-powerline font-meslo-lg font-meslo-for-powerline font-noto-sans font-noto-serif font-source-sans-pro font-source-serif-pro font-ubuntu font-pt-mono font-pt-sans font-pt-serif font-fira-mono font-fira-mono-for-powerline font-fira-code font-fira-sans font-source-code-pro font-hack')
